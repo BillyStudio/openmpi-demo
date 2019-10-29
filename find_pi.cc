cc85b8b6ecd27c03ffd52680016e33f9137e294f
BillyStudio <shengdian.wang@outlook.com>
2019-10-29 08:44:17 +0800

    modify find_pi for calc time

--- find_pi.cc
+++ find_pi.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    int done = 0, n, myid, numprocs, i;
+    int done = 0, n = 0, myid, numprocs, i;
     double PI25DT = 3.141592653589793238462643;
     double mypi, pi, h, sum, x;
 
@@ -12,27 +12,29 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     while (!done)
     {
-	if (myid == 0) {
-	    printf("Enter the number of intervals: (0 quits) ");
-	    scanf("%d",&n);
-	}
-	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	if (n == 0) break;
-  
-	h   = 1.0 / (double) n;
-	sum = 0.0;
-	for (i = myid + 1; i <= n; i += numprocs) {
-	    x = h * ((double)i - 0.5);
-	    sum += 4.0 / (1.0 + x*x);
-	}
-	mypi = h * sum;
+		if (n < 0) break;
+		if (myid == 0) {
+			//printf("Enter the number of intervals: (0 quits) ");
+			//scanf("%d",&n);
+			n = 1000000;
+		}
+		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	
+		h   = 1.0 / (double) n;
+		sum = 0.0;
+		for (i = myid + 1; i <= n; i += numprocs) {
+			x = h * ((double)i - 0.5);
+			sum += 4.0 / (1.0 + x*x);
+		}
+		mypi = h * sum;
+		
+		MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
+			MPI_COMM_WORLD);
     
-	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
-		   MPI_COMM_WORLD);
-    
-	if (myid == 0)
-	    printf("pi is approximately %.16f, Error is %.16f\n",
-		   pi, fabs(pi - PI25DT));
+		if (myid == 0)
+			printf("pi is approximately %.16f, Error is %.16f\n",
+			pi, fabs(pi - PI25DT));
+		n = -1;
     }
     MPI_Finalize();
     return 0;
